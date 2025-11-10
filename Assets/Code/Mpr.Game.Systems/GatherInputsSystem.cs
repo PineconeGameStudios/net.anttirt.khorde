dc842a60104b9805fb4a4cc052543a50f65343f9
anttirt <anttirt@anttirt.net>
2025-11-10 08:53:55 +0200

    clean up

---
+++ Assets/Code/Mpr.Game.Systems/GatherInputsSystem.cs
@@ -0,0 +1,37 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.NetCode;
+using UnityEngine;
+
+namespace Mpr.Game
+{
+	[UpdateInGroup(typeof(GhostInputSystemGroup))]
+	public partial class GatherInputsSystem : SystemBase
+	{
+		Input.InputActions inputActions;
+
+		protected override void OnCreate()
+		{
+			inputActions = new Input.InputActions();
+			inputActions.Enable();
+
+			RequireForUpdate<PlayerInput>();
+		}
+
+		protected override void OnUpdate()
+		{
+			float2 move = inputActions.Player.Move.ReadValue<Vector2>();
+
+			foreach(var input in SystemAPI.Query<RefRW<PlayerInput>>().WithAll<GhostOwnerIsLocal>())
+			{
+				input.ValueRW.move = move;
+			}
+		}
+
+		protected override void OnDestroy()
+		{
+			inputActions.Disable();
+			inputActions.Dispose();
+		}
+	}
+}
--- Assets/Code/Mpr.Game.Systems/PlayerControllerSystem.cs
+++ Assets/Code/Mpr.Game.Systems/PlayerControllerSystem.cs
@@ -1,55 +1,25 @@
+using Unity.Burst;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.NetCode;
 using Unity.Transforms;
-using UnityEngine;
 
 namespace Mpr.Game
 {
 	[UpdateInGroup(typeof(PredictedSimulationSystemGroup))]
-	public partial class PlayerControllerSystem : SystemBase
+	public partial struct PlayerControllerSystem : ISystem
 	{
-		protected override void OnCreate()
+		void ISystem.OnCreate(ref SystemState state)
 		{
-			RequireForUpdate<PlayerInput>();
+			state.RequireForUpdate<PlayerInput>();
 		}
 
-		protected override void OnUpdate()
+		[BurstCompile]
+		void ISystem.OnUpdate(ref SystemState state)
 		{
 			foreach(var (input, controller, ltw) in SystemAPI.Query<PlayerInput, PlayerController, RefRW<LocalTransform>>())
 			{
 				ltw.ValueRW.Position.xy += input.move * SystemAPI.Time.DeltaTime * controller.speed;
 			}
-		}
-	}
-
-	[UpdateInGroup(typeof(GhostInputSystemGroup))]
-	public partial class GatherInputsSystem : SystemBase
-	{
-		Input.InputActions inputActions;
-
-		protected override void OnCreate()
-		{
-			inputActions = new Input.InputActions();
-			inputActions.Enable();
-
-			RequireForUpdate<PlayerInput>();
-		}
-
-		protected override void OnUpdate()
-		{
-			float2 move = inputActions.Player.Move.ReadValue<Vector2>();
-
-			foreach(var input in SystemAPI.Query<RefRW<PlayerInput>>().WithAll<GhostOwnerIsLocal>())
-			{
-				input.ValueRW.move = move;
-			}
-		}
-
-		protected override void OnDestroy()
-		{
-			inputActions.Disable();
-			inputActions.Dispose();
 		}
 	}
 }
