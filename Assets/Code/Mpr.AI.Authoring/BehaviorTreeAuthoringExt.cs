1c0e911f151d14168d3b30bfbf41b460dfa30720
anttirt <anttirt@anttirt.net>
2025-11-22 15:12:07 +0200

    move blob construction helper stuff to authoring

---
+++ Assets/Code/Mpr.AI.Authoring/BehaviorTreeAuthoringExt.cs
@@ -0,0 +1,39 @@
+using Unity.Entities;
+using static Mpr.AI.BT.BTExec;
+
+namespace Mpr.AI.BT.Test
+{
+	public static class BehaviorTreeAuthoringExt
+	{
+		public static void SetData(ref this BTExec self, in Root value) { self.type = Type.Root; self.data.root = value; }
+		public static void SetSequence(ref this BTExec self, ref BlobBuilder builder, BlobBuilderArray<BTExec> execs, params ushort[] childNodeIds)
+		{
+			self.type = Type.Sequence;
+			var array = builder.Allocate(ref self.data.sequence.children, childNodeIds.Length);
+			for(int i = 0; i < childNodeIds.Length; i++)
+			{
+				array[i] = new BTExecNodeId(childNodeIds[i]);
+				if(execs[childNodeIds[i]].childIndex != 0)
+					throw new System.Exception("node already claimed by another node");
+				execs[childNodeIds[i]].childIndex = (byte)i;
+			}
+		}
+		public static void SetSelector(ref this BTExec self, ref BlobBuilder builder, BlobBuilderArray<BTExec> execs, params (ushort, BTExprNodeRef)[] childNodeIds)
+		{
+			self.type = Type.Selector;
+			var array = builder.Allocate(ref self.data.selector.children, childNodeIds.Length);
+			for(int i = 0; i < childNodeIds.Length; i++)
+			{
+				array[i] = new ConditionalBlock { nodeId = new BTExecNodeId(childNodeIds[i].Item1), condition = childNodeIds[i].Item2 };
+				if(execs[childNodeIds[i].Item1].childIndex != 0)
+					throw new System.Exception("node already claimed by another node");
+				execs[childNodeIds[i].Item1].childIndex = (byte)i;
+			}
+		}
+		public static void SetData(ref this BTExec self, in WriteField value) { self.type = Type.WriteField; self.data.writeField = value; }
+		public static void SetData(ref this BTExec self, in Wait value) { self.type = Type.Wait; self.data.wait = value; }
+		public static void SetData(ref this BTExec self, in Fail value) { self.type = Type.Fail; self.data.fail = value; }
+		public static void SetData(ref this BTExec self, in Optional value) { self.type = Type.Optional; self.data.optional = value; }
+		public static void SetData(ref this BTExec self, in Catch value) { self.type = Type.Catch; self.data.@catch = value; }
+	}
+}
