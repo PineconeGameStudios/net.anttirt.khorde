c28fa43d36159077d38c3101e2031e969c1503f0
anttirt <anttirt@anttirt.net>
2025-11-23 18:55:25 +0200

    graph test execution

--- Assets/Code/Mpr.AI.Authoring/BehaviorTreeNodeMath.cs
+++ Assets/Code/Mpr.AI.Authoring/BehaviorTreeNodeMath.cs
@@ -42,4 +42,83 @@ namespace Mpr.AI.BT.Nodes
 				.Build();
 		}
 	}
+
+	[Serializable]
+	internal class AndBool : Base, IExprNode
+	{
+		public void Bake(ref BlobBuilder builder, ref BTExpr expr, BakingContext context)
+		{
+			expr.type = BTExpr.ExprType.Bool;
+			expr.data.@bool = new BTExpr.Bool
+			{
+				index = BTExpr.Bool.BoolType.And,
+				data = new BTExpr.Bool.Data
+				{
+					and = new BTExpr.Bool.And(
+						context.GetExprNodeRef(GetInputPort(0)),
+						context.GetExprNodeRef(GetInputPort(1))
+						),
+				}
+			};
+		}
+
+		protected override void OnDefinePorts(IPortDefinitionContext context)
+		{
+			context.AddInputPort<bool>("a").WithDisplayName(string.Empty).WithConnectorUI(PortConnectorUI.Circle).Build();
+			context.AddInputPort<bool>("b").WithDisplayName(string.Empty).WithConnectorUI(PortConnectorUI.Circle).Build();
+			context.AddOutputPort<bool>("out").WithDisplayName(string.Empty).WithConnectorUI(PortConnectorUI.Circle).Build();
+		}
+	}
+
+	[Serializable]
+	internal class OrBool : Base, IExprNode
+	{
+		public void Bake(ref BlobBuilder builder, ref BTExpr expr, BakingContext context)
+		{
+			expr.type = BTExpr.ExprType.Bool;
+			expr.data.@bool = new BTExpr.Bool
+			{
+				index = BTExpr.Bool.BoolType.Or,
+				data = new BTExpr.Bool.Data
+				{
+					or = new BTExpr.Bool.Or(
+						context.GetExprNodeRef(GetInputPort(0)),
+						context.GetExprNodeRef(GetInputPort(1))
+						),
+				}
+			};
+		}
+
+		protected override void OnDefinePorts(IPortDefinitionContext context)
+		{
+			context.AddInputPort<bool>("a").WithDisplayName(string.Empty).WithConnectorUI(PortConnectorUI.Circle).Build();
+			context.AddInputPort<bool>("b").WithDisplayName(string.Empty).WithConnectorUI(PortConnectorUI.Circle).Build();
+			context.AddOutputPort<bool>("out").WithDisplayName(string.Empty).WithConnectorUI(PortConnectorUI.Circle).Build();
+		}
+	}
+
+	[Serializable]
+	internal class NotBool : Base, IExprNode
+	{
+		public void Bake(ref BlobBuilder builder, ref BTExpr expr, BakingContext context)
+		{
+			expr.type = BTExpr.ExprType.Bool;
+			expr.data.@bool = new BTExpr.Bool
+			{
+				index = BTExpr.Bool.BoolType.Not,
+				data = new BTExpr.Bool.Data
+				{
+					not = new BTExpr.Bool.Not(
+						context.GetExprNodeRef(GetInputPort(0))
+						),
+				}
+			};
+		}
+
+		protected override void OnDefinePorts(IPortDefinitionContext context)
+		{
+			context.AddInputPort<bool>("in").WithDisplayName(string.Empty).WithConnectorUI(PortConnectorUI.Circle).Build();
+			context.AddOutputPort<bool>("out").WithDisplayName(string.Empty).WithConnectorUI(PortConnectorUI.Circle).Build();
+		}
+	}
 }
--- Assets/Code/Mpr.AI.Test/BehaviorTreeGraphTests.cs
+++ Assets/Code/Mpr.AI.Test/BehaviorTreeGraphTests.cs
@@ -1,9 +1,10 @@
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEditor;
+using static Mpr.AI.BT.BTExec;
+using static Mpr.AI.BT.BTExecTrace;
 
 namespace Mpr.AI.BT.Test
 {
@@ -43,29 +44,67 @@ namespace Mpr.AI.BT.Test
 				var dump = new List<string>();
 				BehaviorTreeExecution.DumpNodes(ref data.Value, dump);
 
-				foreach(var line in dump)
-					UnityEngine.Debug.Log(line);
+				//foreach(var line in dump)
+				//	UnityEngine.Debug.Log(line);
 
-				Span<UnsafeComponentReference> comps = stackalloc UnsafeComponentReference[2];
+				System.Span<UnsafeComponentReference> comps = stackalloc UnsafeComponentReference[2];
 				comps[0] = UnsafeComponentReference.Make(ref moveTarget);
 				comps[1] = UnsafeComponentReference.Make(ref localTransform);
 
-				BehaviorTreeExecution.Execute(
-					data,
-					ref state,
-					stack,
-					comps,
-					0,
-					trace);
+				BehaviorTreeExecution.Execute(data, ref state, stack, comps, 0, trace);
+
+				AssertTrace
+				(
+					Trace(Type.Root, 1, 0, Event.Init),
+					Trace(Type.Root, 1, 1, Event.Start),
+					Trace(Type.Root, 1, 1, Event.Call),
+					Trace(Type.Sequence, 2, 2, Event.Call),
+					Trace(Type.Sequence, 5, 3, Event.Call),
+					Trace(Type.WriteField, 3, 4, Event.Return),
+					Trace(Type.Sequence, 5, 3, Event.Call),
+					Trace(Type.Wait, 4, 4, Event.Wait)
+				);
+
+				trace.Clear();
+
+				BehaviorTreeExecution.Execute(data, ref state, stack, comps, 0, trace);
+
+				AssertTrace
+				(
+					Trace(Type.Wait, 4, 4, Event.Start),
+					Trace(Type.Wait, 4, 4, Event.Wait)
+				);
+
+				trace.Clear();
+
+				moveTarget.enabled = false;
+
+				BehaviorTreeExecution.Execute(data, ref state, stack, comps, 0, trace);
+
+				AssertTrace
+				(
+					Trace(Type.Wait, 4, 4, Event.Start),
+					Trace(Type.Wait, 4, 4, Event.Return),
+					Trace(Type.Sequence, 5, 3, Event.Return),
+					Trace(Type.Sequence, 2, 2, Event.Call),
+					Trace(Type.Sequence, 8, 3, Event.Call),
+					Trace(Type.WriteField, 6, 4, Event.Return),
+					Trace(Type.Sequence, 8, 3, Event.Call),
+					Trace(Type.Wait, 7, 4, Event.Wait)
+				);
+
 			}
 			finally
 			{
 				if(data.IsCreated)
 					data.Dispose();
-
-				UnityEngine.Debug.Log(string.Join("\n", trace.AsNativeArray().ToArray()));
 			}
 		}
+
+		void AssertTrace(params BTExecTrace[] expected) => Assert.AreEqual(expected, trace.AsNativeArray().AsSpan().ToArray());
+
+		static BTExecTrace Trace(Type type, ushort nodeId, int depth, Event @event)
+			=> new BTExecTrace(new BTExecNodeId(nodeId), type, @event, depth, 0);
 
 		[TearDown]
 		public void TearDown()
