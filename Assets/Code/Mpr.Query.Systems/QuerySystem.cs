ffb9cbfff759c1d742ad5178d4624f6b11bab824
anttirt <anttirt@anttirt.net>
2025-12-01 14:15:13 +0200

    add stub query system

--- Assets/Code/Mpr.Query.Systems/QuerySystem.cs
+++ Assets/Code/Mpr.Query.Systems/QuerySystem.cs
@@ -1,9 +1,36 @@
+using Mpr.Expr;
+using System;
+using Unity.Burst;
+using Unity.Burst.Intrinsics;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Mpr.Query
 {
-    public class QuerySystem
-    {
-    
-    }
+	public partial struct QuerySystem : ISystem
+	{
+		[BurstCompile]
+		partial struct QueryJob : IJobEntity
+		{
+			public void Execute(Query query)
+			{
+				if(!query.query.IsCreated)
+					return;
+				Span<UnsafeComponentReference> components = stackalloc UnsafeComponentReference[1];
+				QueryExecution.Execute<float2>(ref query.query.Value, components, Allocator.Temp);
+			}
+		}
+
+		void ISystem.OnCreate(ref SystemState state)
+		{
+			state.RequireForUpdate<Query>();
+		}
+
+		void ISystem.OnUpdate(ref SystemState state)
+		{
+			new QueryJob().Schedule();
+		}
+	}
 }
