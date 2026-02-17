a54a51aab1aea2f9074909b0bcdcbd9f1df8009c
anttirt <anttirt@anttirt.net>
2026-02-17 21:53:50 +0200

    c# 9 compat

--- Packages/net.anttirt.khord/Mpr.Lang/IsExternalInit.cs
+++
@@ -1,7 +0,0 @@
-namespace System.Runtime.CompilerServices
-{
-	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-	public sealed class IsExternalInit : Attribute
-	{
-	}
-}
