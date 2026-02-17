55482683dc49fac1c192c75cddec23987b88f8f4
anttirt <anttirt@anttirt.net>
2026-02-17 20:59:12 +0200

    windows player build

--- Assets/Code/Mpr.Game/UI/SceneButton.cs
+++ Assets/Code/Mpr.Game/UI/SceneButton.cs
@@ -1,15 +1,16 @@
 using Unity.Entities.Content;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Mpr.Game.UI
 {
 	class SceneButton : MonoBehaviour
 	{
-		public WeakObjectSceneReference scene;
+		public string sceneName;
 
 		public void OnClick()
 		{
-			scene.LoadAsync(new Unity.Loading.ContentSceneParameters { loadSceneMode = UnityEngine.SceneManagement.LoadSceneMode.Single, autoIntegrate = true });
+			SceneManager.LoadScene(sceneName);
 		}
 	}
 }
